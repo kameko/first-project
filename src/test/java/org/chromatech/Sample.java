a75ffafb819067203584fe65ae65cf0fb478350f
Kameko Kirishiki <kameko.k@outlook.com>
2025-02-06 20:38:47 -0500

    New line again

--- src/test/java/org/chromatech/Sample.java
+++ src/test/java/org/chromatech/Sample.java
@@ -4,5 +4,6 @@ public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
         System.out.println("Making a change after pushing project.");
+        System.out.println("Adding another new line.");
     }
 }
