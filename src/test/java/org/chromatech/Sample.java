628df000f362f63bc3dabdca6bacdf20e3ba0341
Kameko Kirishiki <kameko.k@outlook.com>
2025-02-06 19:47:05 -0500

    Added a line of code to the Sample class

--- src/test/java/org/chromatech/Sample.java
+++ src/test/java/org/chromatech/Sample.java
@@ -3,5 +3,6 @@ package org.chromatech;
 public class Sample {
     public static void main(String[] args) {
         System.out.println("Hello, world!");
+        System.out.println("Making a change after pushing project.");
     }
 }
